d762305f15a8050cfcc78c99ddcf3be1c80f21a4
KaykeTorres-Dev <kayketorres72@gmail.com>
2024-06-19 10:57:25 -0300

    Criação das galerias de imagens aprovadas e reprovadas, junto com a modal para visualizar as imagens

--- src/app/private/imagens-aprovadas/imagens-aprovadas.component.ts
+++ src/app/private/imagens-aprovadas/imagens-aprovadas.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { DashboardComponent } from '../dashboard/dashboard.component';
+import { CommonModule } from '@angular/common';
+import { ModalComponent } from '../shared/modal/modal.component';
 
 @Component({
   selector: 'app-imagens-aprovadas',
   standalone: true,
-  imports: [DashboardComponent],
+  imports: [DashboardComponent, CommonModule, ModalComponent],
   templateUrl: './imagens-aprovadas.component.html',
   styleUrl: './imagens-aprovadas.component.scss'
 })
 export class ImagensAprovadasComponent {
-
+  imagensAprovadas = [
+    {id: 1, imagePath: 'assets/camera-images/aprovadas/image1.bmp', titulo: 'Imagem 1'},
+    {id: 2, imagePath: 'assets/camera-images/aprovadas/image1.bmp', titulo: 'Imagem 2'},
+    {id: 3, imagePath: 'assets/camera-images/aprovadas/image1.bmp', titulo: 'Imagem 3'},
+    {id: 4, imagePath: 'assets/camera-images/aprovadas/image1.bmp', titulo: 'Imagem 4'},
+    {id: 5, imagePath: 'assets/camera-images/aprovadas/image1.bmp', titulo: 'Imagem 5'},
+    {id: 6, imagePath: 'assets/camera-images/aprovadas/image1.bmp', titulo: 'Imagem 6'},
+    {id: 7, imagePath: 'assets/camera-images/aprovadas/image1.bmp', titulo: 'Imagem 7'},
+    {id: 8, imagePath: 'assets/camera-images/aprovadas/image1.bmp', titulo: 'Imagem 8'},
+    {id: 9, imagePath: 'assets/camera-images/aprovadas/image1.bmp', titulo: 'Imagem 9'},
+    {id: 10, imagePath: 'assets/camera-images/aprovadas/image1.bmp', titulo: 'Imagem 10'},
+    {id: 11, imagePath: 'assets/camera-images/aprovadas/image1.bmp', titulo: 'Imagem 11'},
+    {id: 12, imagePath: 'assets/camera-images/aprovadas/image1.bmp', titulo: 'Imagem 12'}
+  ];
 }
+
+
+
