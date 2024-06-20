295b29c8d89903dabb1005e6840d97be86e6fa9d
KaykeTorres-Dev <kayketorres72@gmail.com>
2024-06-20 17:22:46 -0300

    Criação do authguard, criação do auth-service para autenticar o usuário e ajuste no carrousel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './public/login/login.component'; 
+import { LoginComponent } from './public/login/login.component';
 import { HomeComponent } from './private/home/home.component';
 import { ImagensAprovadasComponent } from './private/imagens-aprovadas/imagens-aprovadas.component';
 import { ImagensReprovadasComponent } from './private/imagens-reprovadas/imagens-reprovadas.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent }, 
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'imagens-aprovadas', component: ImagensAprovadasComponent },
-  { path: 'imagens-reprovadas', component: ImagensReprovadasComponent }, 
+  { path: 'imagens-reprovadas', component: ImagensReprovadasComponent },
+  {
+    path: '**',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-
-export class AppRoutes { }
+export class AppRoutes {}
