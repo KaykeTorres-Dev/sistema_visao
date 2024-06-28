12d580ce0c4704768f0371bbe61b2c843b139005
KaykeTorres-Dev <kayketorres72@gmail.com>
2024-06-28 15:31:33 -0300

    Implementação do layout em produtos e correção de texto na dashboard

--- src/app/private/lista-produtos/produtos/produtos.component.ts
+++ src/app/private/lista-produtos/produtos/produtos.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-produtos',
@@ -10,4 +11,45 @@ import { DashboardComponent } from '../../dashboard/dashboard.component';
 })
 export class ProdutosComponent {
 
+  constructor(public router: Router) {}
+
+  goToProduct1() {
+    this.router.navigate(['produtos/quantificacao-de-garrafas-em-engradados']);
+  }
+
+  goToProduct2() {
+    this.router.navigate(['produtos/verificacao-de-rosca-interna-e-externa']);
+  }
+
+  goToProduct3() {
+    this.router.navigate(['produtos/qualidade-e-medicao-de-pneus']);
+  }
+
+  goToProduct4() {
+    this.router.navigate(['produtos/medicao-de-volume-em-processo-continuo']);
+  }
+
+  goToProduct5() {
+    this.router.navigate(['produtos/controle-de-qualidade-na-fabricacao-de-biscoitos']);
+  }
+
+  goToProduct6() {
+    this.router.navigate(['produtos/inspecao-de-rotulos-tampas-e-nivel-de-produto']);
+  }
+
+  goToProduct7() {
+    this.router.navigate(['produtos/conferencia-de-embalagens']);
+  }
+
+  goToProduct8() {
+    this.router.navigate(['produtos/identificacao-de-irregularidades-em-bocal-ou-tampas']);
+  }
+
+  goToProduct9() {
+    this.router.navigate(['produtos/leitor-de-caracteres-em-qualquer-material']);
+  }
+
+  goToProduct10() {
+    this.router.navigate(['produtos/maquina-de-cubagem-pesagem-e-identificacao']);
+  }
 }
