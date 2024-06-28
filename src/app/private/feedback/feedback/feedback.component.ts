28088558b4e5b954aa63ff49b74042d0c288af9f
KaykeTorres-Dev <kayketorres72@gmail.com>
2024-06-28 11:22:37 -0300

    Implementação da dashboard em feedback e o direcionamento de sua rota ao clicar no botão feedback em home.

--- src/app/private/feedback/feedback/feedback.component.ts
+++ src/app/private/feedback/feedback/feedback.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DashboardComponent } from '../../dashboard/dashboard.component';
 
 @Component({
   selector: 'app-feedback',
   standalone: true,
-  imports: [],
+  imports: [DashboardComponent],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.scss'
 })
