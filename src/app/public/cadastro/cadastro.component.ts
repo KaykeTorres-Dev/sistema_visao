8cb6bda79870c62abbb2997912cab3147cfc6a3e
KaykeTorres-Dev <kayketorres72@gmail.com>
2024-07-15 17:21:31 -0300

    Implementação das validações no formulário de cadastro e ajuste em login

--- src/app/public/cadastro/cadastro.component.ts
+++ src/app/public/cadastro/cadastro.component.ts
@@ -17,7 +17,13 @@ export class CadastroComponent implements OnInit {
   passwordShow: boolean = false;
   confirmPasswordShow: boolean = false;
   confirmPasswordType: string = 'password';
-  isPasswordMatch: boolean = false;
+  isPasswordMatch: boolean | undefined;
+  isFormInvalid: boolean | undefined;
+  isEmailInvalid: boolean | undefined;
+  isCompanyInvalid: boolean = false;
+  isPasswordInvalid: boolean = false;
+  isConfirmPasswordInvalid: boolean = false;
+  isConfirmPasswordEmpty: boolean = false;
 
   constructor(
     private fb: FormBuilder,
@@ -29,11 +35,24 @@ export class CadastroComponent implements OnInit {
   ngOnInit(): void {
     this.form = this.fb.group({
       email: new FormControl (null, [Validators.required, Validators.email]),
-      empresa: new FormControl (null, Validators.required),
-      senha: new FormControl (null, [Validators.required, Validators.minLength(6)]),
-      confirmarSenha: new FormControl (null, [Validators.required, Validators.minLength(6)])
+      company: new FormControl (null, Validators.required),
+      password: new FormControl (null, [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl (null, Validators.required)
     });
   }
+
+  checkEmail(email: string) {
+    if (email.length > 0 && (email !== null || email !== '')) {
+      if (this.form.get('email')?.valid) {
+        this.isEmailInvalid = false;
+      } else {
+        this.isEmailInvalid = true;
+      }
+    } else {
+      this.isEmailInvalid = false;
+    }
+  }
+
 
   togglePassword() {
     if (this.passwordShow) {
@@ -57,25 +76,84 @@ export class CadastroComponent implements OnInit {
 
   passwordMatchCheck(password: any, confirmPassword: any) {
     if (password !== null && password !== '') {
+      this.isConfirmPasswordInvalid = false;
+
       if (confirmPassword.length > 0) {
+        this.isConfirmPasswordEmpty = false;
+
         if (password !== confirmPassword) {
-          this.toastService.showDanger('As senhas não são iguais!');
+          console.log('senha', password, 'confirmarSenha', confirmPassword);
+          this.isPasswordMatch = false;
+
+        } else {
+          this.isPasswordMatch = true;
+        }
+      } else {
+        this.isPasswordMatch = undefined;
+        this.isConfirmPasswordEmpty = true;
+      }
+    } else {
+      this.isPasswordMatch = undefined;
+      this.isConfirmPasswordInvalid = true;
+      this.toastService.showDanger('Preencha a senha primeiro!');
+      this.isConfirmPasswordEmpty = true;
+    }
+  }
+
+  checkPassword(password: any) {
+    const confirmPassword = this.form.get('confirmPassword')?.value;
+
+    if (password !== null && password !== '') {
+      if (password.length > 0 && password.length < 6) {
+        this.isPasswordInvalid = true;
+
+      } else if (password.length >= 6) {
+        this.isPasswordInvalid = false;
+
+      } if (confirmPassword !== null && confirmPassword !== '') {
+        if (password !== confirmPassword) {
           this.isPasswordMatch = false;
         } else {
           this.isPasswordMatch = true;
         }
       }
+
+    } else if (confirmPassword !== null && confirmPassword !== '') {
+      this.isPasswordInvalid = false;
+      this.isFormInvalid = false;
     } else {
-      this.toastService.showDanger('Preencha a senha primeiro!');
+      this.isPasswordInvalid = false;
+      this.isConfirmPasswordEmpty = false;
     }
   }
 
+  validateCreateUserForm(email: string, company: string, password: string, confirmPassword: string): string | null {
+    if ((email === null || email === '') && (company === null || company === '') && (password === null || password === '')
+      && (confirmPassword === null || confirmPassword === '')) {
+      return 'Preencha os campos abaixo!';
+
+    } else if ((email === null || email === '') || (company === null || company === '') || (password === null || password === '')
+      || (confirmPassword === null || confirmPassword === '')) {
+      return 'Formulário inválido!';
+    }
+    return null;
+  }
+
   createUser() {
-    if (!this.form.valid || !this.isPasswordMatch) {
-      this.toastService.showDanger('Formulário Inválido!');
+    const email = this.form.get('email')?.value;
+    const company = this.form.get('company')?.value;
+    const password = this.form.get('password')?.value;
+    const confirmPassword = this.form.get('confirmPassword')?.value;
+
+    const validationError = this.validateCreateUserForm(email, company, password, confirmPassword);
+    if (validationError) {
+      this.isFormInvalid = true;
+      this.toastService.showDanger(validationError);
+      return;
     } else {
-      this.toastService.showSuccess('Conta criada com sucesso!');
+      this.isFormInvalid = false;
       this.router.navigate(['/login']);
+      this.toastService.showSuccess('Conta criada com sucesso!');
     }
   }
 }
